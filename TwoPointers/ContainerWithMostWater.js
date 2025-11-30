18f100303a375d2307ad09d78ce1c0d0078c93e6
llfbh33 <aubriewoodbine@yahoo.com>
2025-11-30 16:22:11 -0600

    whiteboarding solutions to trapping rain water

---
+++ TwoPointers/ContainerWithMostWater.js
@@ -0,0 +1,61 @@
+/*
+Container With Most Water
+You are given an integer array heights where heights[i] represents the height of the 
+i
+t
+h
+i 
+th
+  bar.
+
+You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+Example 1:
+
+
+
+Input: height = [1,7,2,5,4,7,3,6]
+
+Output: 36
+Example 2:
+
+Input: height = [2,2,2]
+
+Output: 4
+Constraints:
+
+2 <= height.length <= 1000
+0 <= height[i] <= 1000
+*/
+
+// Two pointer methods are like logic puzzles where the pointers take care of the logic
+// we do not need to check all possible combinations like we would with a nested loop because
+    // the two pointers will rule out combinations which dont match
+// move over the lower side because we will not be able to get a larger area if we are reducing in the index and in the 
+
+
+function maxArea(heights) {
+    // max variable which holds the max area - 0
+    let area = 0;                                                                   // --> 36
+    // create a pointer for left
+    let l = 0;                                                                      // --> 1
+    // create a pointer for right
+    let r = heights.length - 1;                                                     // --> 6
+    // while left is less than right (walk through all the options)
+    while (l < r) {
+        // figure out which number is the shorter number
+        let shortest = heights[l] <= heights[r] ? heights[l] : heights[r];          // 7 <= 3  --> 3
+        // figure out the difference between the two indices
+        let newArea = shortest * (r - l);                                           // 3 * 5 = 15
+        // if the shortest number * the difference (area) is greater than max, replace max with area;
+        if (newArea > area) area = newArea;
+        // if left is shorter than right, move left once
+        // if right is shorter than left, move right once
+        heights[l] <= heights[r] ? l++ : r--;
+    }
+    // return max
+    return area;
+}
+
+let height = [1,7,2,5,4,7,3,6];   // --> 36
+console.log(maxArea(height))
---
+++ TwoPointers/TrappingRainWater.js
@@ -0,0 +1,33 @@
+/*
+Trapping Rain Water
+You are given an array of non-negative integers height which represent an elevation map. Each value height[i] represents the height of a bar, which has a width of 1.
+
+Return the maximum area of water that can be trapped between the bars.
+
+Example 1:
+
+
+
+Input: height = [0,2,0,3,1,0,1,3,2,1]
+
+Output: 9
+Constraints:
+
+1 <= height.length <= 1000
+0 <= height[i] <= 1000
+
+*/
+
+// We use a two pointer system to work inwards to make sure we are holding water properly
+
+let heights = [3,2,1,5,1,2,3,1,2]
+
+
+function trap(height) {
+    // make a response variable
+    // make a right ponter
+    // make a left pointer
+    // make a possible right ponter
+    // make a possible left pointer
+    // make a while loop condition  
+}
