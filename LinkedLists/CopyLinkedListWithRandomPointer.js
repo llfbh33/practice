609c0a4fafbb4e2434e5e341dc8baff36c2c5542
llfbh33 <aubriewoodbine@yahoo.com>
2026-01-24 13:31:13 -0600

    Worked on and solved problem Copy Linked List with Random Pointer, Estimated Big O

--- LinkedLists/CopyLinkedListWithRandomPointer.js
+++ LinkedLists/CopyLinkedListWithRandomPointer.js
@@ -43,3 +43,59 @@ random is null or is pointing to some node in the linked list.
     // while looping map each deep copy node in a hashmap with a key of the og node and the copy node as the value - because some nodes could have the same val
 // loop through the input list again, for each random that is not null
     // find the coresponding node in the hashmap and add it to the current node in the deep copy
+
+class Node {
+    constructor(val, next = null, random = null) {
+        this.val = val;
+        this.next = next;
+        this.random = random;
+    }
+}
+
+
+let copyRandomList = (head) => {
+    let map = new Map();                                // even though an object can be called a map, you have to use an actual Map to use objects as keys
+    let store = head;                                   // Store is our pointer that will move, head keeps track of the head of the node 
+    let result = new Node(0, null, null);               // first while loop will fully fill result with all nodes without random
+    let temp = result;                                  // temp is the pointer at the tail of result
+
+    while (store !== null) {
+        temp.next = new Node(store.val, null, null);    // have to update temp.next, if we updated temp itself we get an error, there are no connections made
+        map.set(store, temp.next);
+
+        temp = temp.next;
+        store = store.next;
+    };
+    temp = result.next;                                         // reset temp to the head of result
+
+    while (head !== null) {                                     // walk through head and add in random connections
+        // find heads random
+        let random = head.random;
+        // find the random in the map
+        if (map.get(random) !== undefined) {                        // leave alone if undefined, all randoms start as null - can not use square bracket notation to access information within Maps
+                // map at random is an already created deep copy
+                // add the value to temp
+            temp.random = map.get(random);
+        };
+        // advance temp and head
+        temp = temp.next;
+        head = head.next;
+    };
+
+    return result.next;
+};
+// Time of O(n) - Map.get has a time complexity of O(1) as it will jump directly to the bucket searched for, besides that we use loops but nothing nested
+    // it would only be O(n²) if we were iterating through all the keys
+// Space of O(n) since we are using a map and will store n amount of entries, one per node
+// With space we talk about Auxiliary Space (extra space beyond the input) so that means the coppied nodes and the map count over constant O(1)
+
+
+let node = new Node(3, 7, null);
+node.next = new Node(7, 4, 5);
+node.next.next = new Node(4, 5, 3);
+node.next.next.next = new Node(5, null, 7);
+// node.next.next.next.next = new Node(4);
+// node.next.next.next.next.next = new Node(5);
+// node.next.next.next.next.next.next = new Node(6);
+
+console.log(copyRandomList(node));
