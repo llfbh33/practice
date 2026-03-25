b461d855ae4a50968d79b2dc4613dbeaed316615
llfbh33 <aubriewoodbine@yahoo.com>
2026-03-25 15:36:26 -0500

    walking through adding types within function and changinging dependedt types based on output type

--- TypeScriptPractice/PracticeOne.ts
+++ TypeScriptPractice/PracticeOne.ts
@@ -15,3 +15,19 @@ console.log(cat2)
 // This is only a TypeScript Phrase
 let dog: string = "They are so friendly!!"
 console.log(dog);
+
+
+// When we have a function in TypeScript we need to include types for each of the parameters provided
+// We also need to add a type after the parameters for that the type returned from the function will be
+const newName = (firstName: string, middleName: string, lastName: string): string => {
+    return `My new name is ${firstName} ${middleName} ${lastName}`
+};
+function newAge(oldAge: number, changedAge: number): string {
+    return `I used to be ${oldAge}, but I decided at heart I am ${changedAge}`
+};
+// The declaration can work with arrow functions as well as traditional functions
+
+// However, with functions Inferred types are often prefered over explicit function returns like above
+// in many scenarios because of improved maintainability, better refactoring experience, and reduced 
+// code verbosity. Explicit types, while having some advantaged, introduce maintenance overhead, especaly 
+// when dependent types may change.
