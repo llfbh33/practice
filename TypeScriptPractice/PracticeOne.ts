61dda7835dd4cc841d2cc4e7c30fed2aa0bdcdd5
llfbh33 <aubriewoodbine@yahoo.com>
2026-03-25 15:22:08 -0500

    understanding the static nature of TypeScript and naming conventions for variables

--- TypeScriptPractice/PracticeOne.ts
+++ TypeScriptPractice/PracticeOne.ts
@@ -2,10 +2,15 @@
 // Anything you can type in javaScript can be converted to typeScript
 // But not all TypeScript can be converted to JavaScript
 
+// TypeScripts Purpose is to add static types to JavaScript
 
-// This is a JavaScript and TypeScript phrase
+
+// This is a JavaScript and TypeScript phrase, But the type of cat is "any"
 let cat = 'So Fluffy!!'
 console.log(cat);
+// When converted to .ts from .js it will be updated to (added 2 in here so that we do not have page errors)
+let cat2: any = "So Fluffy!!"
+console.log(cat2)
 
 // This is only a TypeScript Phrase
 let dog: string = "They are so friendly!!"
