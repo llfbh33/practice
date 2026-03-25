db27e98684191e05de03c59a403613e6695bfeba
llfbh33 <aubriewoodbine@yahoo.com>
2026-03-25 15:54:04 -0500

    processing the idea of string litterals as substrings and how they can be used and mutatec within and outside of TypeScript functions

--- TypeScriptPractice/PracticeOne.ts
+++ TypeScriptPractice/PracticeOne.ts
@@ -31,3 +31,40 @@ function newAge(oldAge: number, changedAge: number): string {
 // in many scenarios because of improved maintainability, better refactoring experience, and reduced 
 // code verbosity. Explicit types, while having some advantaged, introduce maintenance overhead, especaly 
 // when dependent types may change.
+
+// Explicitly returning a value from a function, seperating by a space
+
+function combiningPrompts(systemPrompt: string, userPrompt: string) {
+    return `${systemPrompt}\n${userPrompt}`
+};
+let systemPrompt: string = 'You are a helpful assistant';
+let userPrompt: string = 'How do I reset my password';
+console.log(combiningPrompts(systemPrompt, userPrompt));
+
+
+// There is a type called void, which can be used when a function will not have a return value
+function LogMessage(message: string): void {
+    console.log(message);
+};
+// There is no return from the above function, we set the type of return to void - without this
+// it will return undefined as a default if a return statement is not included
+
+
+// We also have string litteral union types
+let severity: "info" | "warning" | "error"
+// The type is still a string and can be mutated as such, but can be used as a prop without being mutable by inputs
+ function logSystemEvent(event: string, 
+  severity: "info" | "warning" | "error"
+): string {
+  return `SYSTEM ${severity.toUpperCase()}: ${event}`;
+};
+// in the above example when using this function, it will only accept one input, not two, and we could capitalize 
+// the severity or mutate it in other ways within the function if we wished
+
+// String litterals are subtypes of string.  We could also extract them like this:
+type Severity = "info" | "warning" | "error";
+
+export function logSystemEvent2(event: string, severity: Severity): string {
+  return `SYSTEM ${severity.toUpperCase()}: ${event}`;
+};
+// We would take in a prop of Severity, inwhich the input could only be one of the three options
