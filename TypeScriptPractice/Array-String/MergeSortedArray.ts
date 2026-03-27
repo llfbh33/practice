30a6c21e4e8878fa1de7b1bfc2c66aaebfbdc083
llfbh33 <aubriewoodbine@yahoo.com>
2026-03-27 18:10:57 -0500

    worked on merge Sorted Array in typescript

--- TypeScriptPractice/Array-String/MergeSortedArray.ts
+++ TypeScriptPractice/Array-String/MergeSortedArray.ts
@@ -16,5 +16,19 @@ final array should not be returned, it just has to store all the numbers in orde
 */
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    // 
+        // loop from back to front nums1 length
+    for(let i: number = nums1.length - 1; i >= 0; i--) {
+    // condition for if m <= 0
+        // set nums1[i] to nums2[n - 1]
+        // decrease n by 1
+    // condition for if n <= 0
+        // set nums1[i] to nums1[m - 1]
+        // decrease m by 1
+    // if nums1[m - 1] greater than or equal to nums2[n - 1]
+        // set nums1[i] to nums1[m - 1]
+        // decrease m
+    // if nums2[n - 1] greater than num1[m - 1]
+        // set nums1[i] to nums2[n - 1]
+        // decrease n
+    }
 }
