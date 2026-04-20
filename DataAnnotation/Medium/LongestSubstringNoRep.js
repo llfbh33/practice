1b015e4a6198cc9997364fc7b432b7ae3cf4f367
llfbh33 <aubriewoodbine@yahoo.com>
2026-04-20 17:34:48 -0500

    worked on problem Longest Substring without Repeating Characters

---
+++ DataAnnotation/Medium/LongestSubstringNoRep.js
@@ -0,0 +1,29 @@
+/*
+3. Longest Substring Without Repeating Characters
+
+
+input - string
+output - number
+
+find the length of the longest substring without duplicate characters
+
+
+make a result variable at 0
+make a count variable at 0
+make a set for char
+loop through the entire string
+if the char is not in the set 
+    - add to set and increment count
+if the char is in the set 
+    - if count is greater than result, replace result with count
+    - set count to 0
+    - set set to a new set
+return result
+
+
+'abcdbqrs'
+*/
+
+const subString = (n) => {
+    
+}
