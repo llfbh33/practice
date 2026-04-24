d6c8990dfbbae7d1267124dbd442e6681bf9a97e
llfbh33 <aubriewoodbine@yahoo.com>
2026-04-24 13:40:24 -0500

    walked through solve for 3Sum

---
+++ DataAnnotation/Medium/3Sum.js
@@ -0,0 +1,43 @@
+/*
+15. 3Sum
+
+input - array of integers
+output - nested array of triplicates
+
+If we sort the array first we can make sure we do not end up with duplicates
+only loop n times
+if nums[i] is the same as nums[i - 1] continue
+use two pointers one left one right and work our way in the middle to find values which sum up to 0
+move left forward if less than or equal to 0 and right backward if more than 0
+if a triplicate is found we want to increment left and decrement right enough so they do not point
+    to duplicates of their current selves
+
+
+
+[-1, 0, 1, 2, -1, -4]
+
+sorted = [-4, -1, -1, 0, 1, 2]
+
+i = 0  /  left = 1  /  right = 5  /  -3
+    n/a
+i = 0  / left = 2  /  right = 5  /  -3
+    n/a
+i = 0  /  left = 3  /  right = 5  /  -2
+    n/a
+i = 0  /  left = 4  /  right = 5  /  -1
+    n/a
+left === right  continue loop
+i = 1  /  left = 2  /  right = 5  /  0
+    [-1, -1, 2]
+i = 1  /  left = 3  / right = 5  /  1
+    n/a
+i = 1  /  left = 3  /  right = 4  /  0
+    [-1, 0, 1]
+left === right  continue loop
+nums[i] === nums[i - 1] continue loop
+i = 3  /  left = 4  /  right = 5  /  3
+left === right end loop because there are no longer 3 numbers to compare
+return [[-1, -1, 2], [-1, 0, 1]]
+
+
+*/
