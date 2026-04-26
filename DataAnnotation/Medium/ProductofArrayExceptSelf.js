f14448f58444ea772b360cfe6c63f7f9fef0d6e3
llfbh33 <aubriewoodbine@yahoo.com>
2026-04-26 10:41:16 -0500

    walking through prefix and suffix array pattern.

---
+++ DataAnnotation/Medium/ProductofArrayExceptSelf.js
@@ -0,0 +1,32 @@
+/*
+
+238. Product of Array Except Self
+
+input - array
+output - array
+
+
+
+
+
+*/
+
+var productExceptSelf = function(nums) {
+
+    let n = nums.length;
+    let res = new Array(n);
+
+    let pre = 1;
+    for(let i = 0; i < n; i++){
+        res[i] = pre;
+        pre *= nums[i];
+    }
+
+    let suf = 1;
+    for(let i = n - 1; i >= 0; i--){
+        res[i] *= suf;
+        suf *= nums[i];
+    }
+
+    return res;
+};
