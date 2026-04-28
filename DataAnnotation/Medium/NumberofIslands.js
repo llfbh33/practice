58d7c506b49052b78079424afc765e52f002483a
llfbh33 <aubriewoodbine@yahoo.com>
2026-04-28 13:07:02 -0500

    Whiteboarded a solve for leetcode problem number of islands

--- DataAnnotation/Medium/NumberofIslands.js
+++ DataAnnotation/Medium/NumberofIslands.js
@@ -13,4 +13,19 @@ output - number
 
 const numIslands = (grid) => {
     let count = 0;
-}
+
+    // walk through each element of the grid with a nested loop
+    // if you run into a '1' you have an island
+    // add the coordinates to a stack
+    // set those coordinates to '0'
+    // increase count by 1
+    // send stack to a helper function
+    // check all the directions for '1's
+    // add all '1's to the stack
+    // continue with the helper function until the stack is empty, we have removed the first island
+    // return back to the nested loop and continue
+    
+    return count;
+};
+// Time of O(n²) we will be looking at the grid n * m times
+// Space of O(1) 
